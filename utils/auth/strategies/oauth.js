4d186f3da02fa06b656dbd52fb56a1e5a7635bba
Pablo Arak <pablo.arak.vm@gmail.com>
2021-09-26 17:21:13 -0300

    Update URLs

--- utils/auth/strategies/oauth.js
+++ utils/auth/strategies/oauth.js
@@ -1,13 +1,13 @@
-const passport = require("passport");
-const axios = require("axios");
-const boom = require("@hapi/boom");
-const { OAuth2Strategy } = require("passport-oauth");
+const passport = require('passport');
+const axios = require('axios');
+const boom = require('@hapi/boom');
+const { OAuth2Strategy } = require('passport-oauth');
 
-const { config } = require("../../../config");
+const { config } = require('../../../config');
 
-const GOOGLE_AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth";
-const GOOGLE_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token";
-const GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
+const GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/auth';
+const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
+const GOOGLE_USERINFO_URL = 'https://openidconnect.googleapis.com/v1/userinfo';
 
 const oAuth2Strategy = new OAuth2Strategy(
   {
@@ -15,12 +15,12 @@ const oAuth2Strategy = new OAuth2Strategy(
     tokenURL: GOOGLE_TOKEN_URL,
     clientID: config.googleClientId,
     clientSecret: config.googleClientSecret,
-    callbackURL: "/auth/google-oauth/callback"
+    callbackURL: '/auth/google-oauth/callback'
   },
   async (accessToken, refreshToken, profile, cb) => {
     const { data, status } = await axios({
       url: `${config.apiUrl}/api/auth/sign-provider`,
-      method: "post",
+      method: 'post',
       data: {
         name: profile.name,
         email: profile.email,
@@ -37,7 +37,7 @@ const oAuth2Strategy = new OAuth2Strategy(
   }
 );
 
-oAuth2Strategy.userProfile = (accessToken, done) => {
+oAuth2Strategy.userProfile = function (accessToken, done) {
   this._oauth2.get(GOOGLE_USERINFO_URL, accessToken, (err, body) => {
     if (err) {
       return done(err);
@@ -59,4 +59,4 @@ oAuth2Strategy.userProfile = (accessToken, done) => {
   });
 };
 
-passport.use("google-oauth", oAuth2Strategy);
+passport.use('google-oauth', oAuth2Strategy);
